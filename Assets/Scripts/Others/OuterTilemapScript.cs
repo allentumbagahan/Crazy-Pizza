149b3df6b1dcb17002ec7323ee78bf3b07750d6e
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2024-01-29 01:49:37 -0400

    Add Roof

---
+++ Assets/Scripts/Others/OuterTilemapScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OuterTilemapScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("trigger tilemap");
+        gameObject.GetComponent<TilemapRenderer>().enabled = false;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        gameObject.GetComponent<TilemapRenderer>().enabled = true;
+    }
+}
